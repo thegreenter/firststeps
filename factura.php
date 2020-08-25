ef04d6298caafb7676c6e0690df8ff2ab85444e6
Giancarlos Salas <giansalex@gmail.com>
2020-08-24 23:53:41 -0500

    Update factura demo

--- factura.php
+++ factura.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use Greenter\Model\Client\Client;
 use Greenter\Model\Company\Company;
 use Greenter\Model\Company\Address;
-use Greenter\Model\Response\BillResult;
 use Greenter\Model\Sale\Invoice;
 use Greenter\Model\Sale\SaleDetail;
 use Greenter\Model\Sale\Legend;
@@ -20,7 +19,7 @@ $see = require __DIR__.'/config.php';
 $client = new Client();
 $client->setTipoDoc('6')
     ->setNumDoc('20000000001')
-    ->setRznSocial('EMPRESA 1');
+    ->setRznSocial('EMPRESA X');
 
 // Emisor
 $address = new Address();
@@ -28,24 +27,26 @@ $address->setUbigueo('150101')
     ->setDepartamento('LIMA')
     ->setProvincia('LIMA')
     ->setDistrito('LIMA')
-    ->setUrbanizacion('NONE')
-    ->setDireccion('AV LS');
+    ->setUrbanizacion('-')
+    ->setDireccion('Av. Villa Nueva 221')
+    ->setCodLocal('0000'); // Codigo de establecimiento asignado por SUNAT, 0000 de lo contrario.
 
 $company = new Company();
-$company->setRuc('20000000001')
-    ->setRazonSocial('EMPRESA SAC')
-    ->setNombreComercial('EMPRESA')
+$company->setRuc('20123456789')
+    ->setRazonSocial('GREEN SAC')
+    ->setNombreComercial('GREEN')
     ->setAddress($address);
 
 // Venta
 $invoice = (new Invoice())
     ->setUblVersion('2.1')
-    ->setTipoOperacion('0101') // Catalog. 51
-    ->setTipoDoc('01')
+    ->setTipoOperacion('0101') // Venta - Catalog. 51
+    ->setTipoDoc('01') // Factura - Catalog. 01
     ->setSerie('F001')
     ->setCorrelativo('1')
-    ->setFechaEmision(new DateTime())
-    ->setTipoMoneda('PEN')
+    ->setFechaEmision(new DateTime('2020-08-24 13:05:00'))
+    ->setTipoMoneda('PEN') // Sol - Catalog. 02
+    ->setCompany($company)
     ->setClient($client)
     ->setMtoOperGravadas(100.00)
     ->setMtoIGV(18.00)
@@ -53,42 +54,64 @@ $invoice = (new Invoice())
     ->setValorVenta(100.00)
     ->setSubTotal(118.00)
     ->setMtoImpVenta(118.00)
-    ->setCompany($company);
+;
 
 $item = (new SaleDetail())
     ->setCodProducto('P001')
-    ->setUnidad('NIU')
+    ->setUnidad('NIU') // Unidad - Catalog. 03
     ->setCantidad(2)
     ->setDescripcion('PRODUCTO 1')
     ->setMtoBaseIgv(100)
     ->setPorcentajeIgv(18.00) // 18%
     ->setIgv(18.00)
-    ->setTipAfeIgv('10')
+    ->setTipAfeIgv('10') // Gravado Op. Onerosa - Catalog. 07
     ->setTotalImpuestos(18.00)
     ->setMtoValorVenta(100.00)
     ->setMtoValorUnitario(50.00)
-    ->setMtoPrecioUnitario(59.00);
+    ->setMtoPrecioUnitario(59.00)
+;
 
 $legend = (new Legend())
-    ->setCode('1000')
+    ->setCode('1000') // Monto en letras - Catalog. 52
     ->setValue('SON DOSCIENTOS TREINTA Y SEIS CON 00/100 SOLES');
 
 $invoice->setDetails([$item])
-        ->setLegends([$legend]);
+    ->setLegends([$legend]);
 
+// Envío a SUNAT
 $result = $see->send($invoice);
 
-// Guardar XML
+// Guardar XML firmado digitalmente.
 file_put_contents($invoice->getName().'.xml',
-                  $see->getFactory()->getLastXml());
+    $see->getFactory()->getLastXml());
 
+// Verificamos que la conexión con SUNAT fue exitosa.
 if (!$result->isSuccess()) {
-    var_dump($result->getError());
+    // Mostrar error al conectarse a SUNAT.
+    echo 'Codigo Error: '.$result->getError()->getCode();
+    echo 'Mensaje Error: '.$result->getError()->getMessage();
     exit();
 }
 
-/**@var $result BillResult*/
-echo $result->getCdrResponse()->getDescription().PHP_EOL;
-
-// Guardar CDR
+// Guardamos el CDR
 file_put_contents('R-'.$invoice->getName().'.zip', $result->getCdrZip());
+
+// CDR Resultado
+$cdr = $result->getCdrResponse();
+
+$code = (int)$cdr->getCode();
+
+if ($code === 0) {
+    echo 'ESTADO: ACEPTADA'.PHP_EOL;
+} else if ($code >= 4000) {
+    echo 'ESTADO: ACEPTADA CON OBSERVACIONES:'.PHP_EOL;
+    var_dump($cdr->getNotes());
+} else if ($code >= 2000 && $code <= 3999) {
+    echo 'ESTADO: RECHAZADA'.PHP_EOL;
+} else {
+    /* Esto no debería darse */
+    /*code: 0100 a 1999 */
+    echo 'Excepción';
+}
+
+echo $cdr->getDescription().PHP_EOL;
